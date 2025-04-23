24bbf5e7000c6f22e888636856e8ed02875f914f
Nik Nenkov <nik.nenkov359@gmail.com>
2025-04-23 11:16:14 +0300

    added education and key skills

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -44,17 +44,31 @@ export default function Home() {
           <h2>Education</h2>
           <ul>
             <li>
-              <h3>Degree - Institution</h3>
-              <p>Month Year - Month Year</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3>High School of Mathematics and Natural Sciences, Gabrovo, Bulgaria</h3>
+              <p>2005 - 2010</p>
+              <p>
+          Focused on Mathematics, Informatics and English Language. The curriculum emphasized mathematics, with additional skills gained in Visual Studio, HTML, CSS, PHP and C/C++ programming. Participated in drama classes and field trips, enhancing creative expression and broadening perspectives. The first year concentrated on English language proficiency.
+              </p>
+            </li>
+            <li>
+              <h3>Technical University, Gabrovo, Bulgaria</h3>
+              <p>2010 - 2013</p>
+              <p>
+          Studied Electrical Engineering Technologies/Technicians. Covered topics ranging from basic electrical laws to complex circuits, CAD modeling, and coding to support studies. Gained hands-on experience with microchips and large-scale machines. Although studies were not completed due to personal reasons, the knowledge acquired has been invaluable in professional development.
+              </p>
             </li>
           </ul>
         </section>
         <section>
           <h2>Skills</h2>
           <ul>
-            <li>Lorem ipsum dolor sit amet</li>
-            <li>Consectetur adipiscing elit</li>
+            <li>Full-stack development (Java, SpringBoot, JSP, Quarkus, Angular, VueJS, React, TypeScript, Golang, Python)</li>
+            <li>Software testing (E2E, UI, API, unit, integration, mocking)</li>
+            <li>Cloud and DevOps (Docker, Kubernetes, Azure, CI/CD pipelines)</li>
+            <li>System design and debugging (sequence and c4model UML diagrams, log analysis, system tracing)</li>
+            <li>API development and integration (REST, OAuth, MQTT, ETL, SQL, WS, libraries, services)</li>
+            <li>Agile workflows (Jira, Figma, async collaboration with global teams)</li>
+            <li>Technical consulting and team support (mentoring, project optimization)</li>
           </ul>
         </section>
         <section>
