15eaea05f5e67674326e2a7ee83ac2da3fb46db1
Nik Nenkov <nik.nenkov359@gmail.com>
2025-04-23 10:32:13 +0300

    updated resources migrated to new version of dedpendencies and stylez

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXTwitter, faFacebook, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faXTwitter, faFacebook, faLinkedin, faXing } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import "./home.scss";
 
@@ -64,19 +64,24 @@ export default function Home() {
       </main>
       <footer className="footer">
         <ul className="social-links">
-          <li>
+          {/* <li>
             <a href="https://x.com/nik_nenkov" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faXTwitter} />
             </a>
-          </li>
-          <li>
+          </li> */}
+          {/* <li>
             <a href="https://www.facebook.com/profile.php?id=100089466912748" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faFacebook} />
             </a>
+          </li> */}
+          <li>
+            <a href="https://www.linkedin.com/in/nik-nenkov" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/nik-nenkov/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faLinkedin} />
+            <a href="https://www.xing.com/profile/Nik_Nenkov" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faXing} />
             </a>
           </li>
           <li>
