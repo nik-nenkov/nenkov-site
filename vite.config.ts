7dff7145bf98b3f28522b5cfe9c86d9cd5e1d663
Nik Nenkov <nik.nenkov359@gmail.com>
2025-04-23 15:31:12 +0300

    fiuxin new route projects

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
+  base: '/', // Ensure this is set to '/' for root deployment
+  build: {
+    outDir: 'build/client', // This is the default, but confirm it's not changed
+  },
 });
