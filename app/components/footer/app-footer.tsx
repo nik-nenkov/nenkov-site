68a0b4ebc25b8a143dc90ea7680e479d9d5d7e71
Nik Nenkov <nik.nenkov359@gmail.com>
2025-05-09 21:10:38 +0300

    improved accessibility

--- app/components/footer/app-footer.tsx
+++ app/components/footer/app-footer.tsx
@@ -1,49 +1,68 @@
-import "./app-footer.scss"
+import "./app-footer.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXTwitter, faFacebook, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function AppFooter() {
-    return (
-        
-      <footer>
+  return (
+    <footer>
       <ul className="social-links">
-        {/* <li>
-          <a href="https://x.com/nik_nenkov" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faXTwitter} />
-          </a>
-        </li> */}
-        {/* <li>
-          <a href="https://www.xing.com/profile/Nik_Nenkov" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faXing} />
-          </a>
-        </li> */}
+        {/* LinkedIn */}
         <li>
-          <a href="https://www.linkedin.com/in/nik-nenkov" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.linkedin.com/in/nik-nenkov"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Visit my LinkedIn profile"
+          >
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
         </li>
+
+        {/* Facebook */}
         <li>
-          <a href="https://www.facebook.com/profile.php?id=100089466912748" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.facebook.com/profile.php?id=100089466912748"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Visit my Facebook profile"
+          >
             <FontAwesomeIcon icon={faFacebook} />
           </a>
         </li>
+
+        {/* GitHub */}
         <li>
-          <a href="https://github.com/nik-nenkov" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/nik-nenkov"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Visit my GitHub profile"
+          >
             <FontAwesomeIcon icon={faGithub} />
           </a>
         </li>
+
+        {/* Email */}
         <li>
-          <a href="mailto:nik.nenkov@icloud.com">
+          <a
+            href="mailto:nik.nenkov@icloud.com"
+            aria-label="Send me an email at nik.nenkov@icloud.com"
+          >
             <FontAwesomeIcon icon={faEnvelope} />
           </a>
         </li>
+
+        {/* Phone */}
         <li>
-          <a href="tel:+359890138750">
+          <a
+            href="tel:+359890138750"
+            aria-label="Call me at +359 890 138 750"
+          >
             <FontAwesomeIcon icon={faPhone} />
           </a>
         </li>
       </ul>
     </footer>
-    );
-}
+  );
+} 
