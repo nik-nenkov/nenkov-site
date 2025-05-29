fdfdb60a52d19e6627eeef694febee0950abeeee
Nik Nenkov <nik.nenkov@icloud.com>
2025-05-29 04:27:39 +0300

    new package updates incl

--- app/components/footer/social-links.tsx
+++ app/components/footer/social-links.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./social-links.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXTwitter, faFacebook, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faXTwitter, faFacebook, faLinkedin, faGithub, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function SocialLinks() {
@@ -19,7 +19,7 @@ export default function SocialLinks() {
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
         </li>
-        <li>
+        {/* <li>
           <a
             href="https://www.facebook.com/profile.php?id=100089466912748"
             target="_blank"
@@ -28,7 +28,7 @@ export default function SocialLinks() {
           >
             <FontAwesomeIcon icon={faFacebook} />
           </a>
-        </li>
+        </li> */}
         <li>
           <a
             href="https://github.com/nik-nenkov"
@@ -37,6 +37,16 @@ export default function SocialLinks() {
             aria-label="Visit my GitHub profile"
           >
             <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/@NikNenkov"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Visit my YouTube channel"
+          >
+            <FontAwesomeIcon icon={faYoutube} />
           </a>
         </li>
         <li>
